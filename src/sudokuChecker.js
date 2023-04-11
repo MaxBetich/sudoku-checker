7920af98759acbf9d3e2dfc87b1fc8b3acbc710b
Max Betich <betichm@gmail.com>
2023-04-11 11:30:01 -0700

    improve Puzzle constructor function

--- src/sudokuChecker.js
+++ src/sudokuChecker.js
@@ -1,3 +1,11 @@
-export default function Puzzle(inputArray1) {
+export default function Puzzle(inputArray1, inputArray2, inputArray3, inputArray4, inputArray5, inputArray6, inputArray7, inputArray8, inputArray9) {
   this.array1 = inputArray1;
+  this.array2 = inputArray2;
+  this.array3 = inputArray3;
+  this.array4 = inputArray4;
+  this.array5 = inputArray5;
+  this.array6 = inputArray6;
+  this.array7 = inputArray7;
+  this.array8 = inputArray8;
+  this.array9 = inputArray9;
 };
