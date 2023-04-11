2e66ebd29f52a8d8e7d31ae27665654386ae3c59
Max Betich <betichm@gmail.com>
2023-04-11 11:24:17 -0700

    add additional Puzzle testing

--- __tests__/sudokuChecker.test.js
+++ __tests__/sudokuChecker.test.js
@@ -7,4 +7,17 @@ describe('Puzzle', () => {
     const newPuzzle = new Puzzle(array);
     expect(newPuzzle.array1).toEqual([1]);
   });
+
+  test('should successfully create a Puzzle object with array1-9 properties', () => {
+    const newPuzzle = new Puzzle([1], [2], [3], [4], [5], [6], [7], [8], [9]);
+    expect(newPuzzle.array1).toEqual([1]);
+    expect(newPuzzle.array2).toEqual([2]);
+    expect(newPuzzle.array3).toEqual([3]);
+    expect(newPuzzle.array4).toEqual([4]);
+    expect(newPuzzle.array5).toEqual([5]);
+    expect(newPuzzle.array6).toEqual([6]);
+    expect(newPuzzle.array7).toEqual([7]);
+    expect(newPuzzle.array8).toEqual([8]);
+    expect(newPuzzle.array9).toEqual([9]);
+  });
 })
